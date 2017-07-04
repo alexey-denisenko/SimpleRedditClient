c2036f1543c580798d7b26ae67172457d3bc01f7
Alexey <likezsmj@gmail.com>
2017-07-04 21:37:17 +0300

    Added TopListContract

--- app/src/main/java/com/denisenko/alexey/simple/reddit/client/top/mvp/TopListContract.java
+++ app/src/main/java/com/denisenko/alexey/simple/reddit/client/top/mvp/TopListContract.java
@@ -1,5 +1,7 @@
 package com.denisenko.alexey.simple.reddit.client.top.mvp;
 
+import android.os.Bundle;
+
 import com.denisenko.alexey.simple.reddit.client.common.BaseContract;
 import com.denisenko.alexey.simple.reddit.client.pojo.RedditChildren;
 
@@ -11,15 +13,28 @@ public interface TopListContract {
 
     interface View {
 
+        void setRefreshing();
+
+        void addItems();
+
+        void showError();
+
+        void stopPagination();
     }
 
     interface Presenter extends BaseContract.Presenter {
+
         void onLoadNextPage();
 
+        void onRefresh();
+
+        void onCreate(Bundle savedInstanceState);
+
+        void onSaveInstanceState(Bundle outState);
     }
 
     interface Model {
-        Observable<List<RedditChildren>> getTopEntriesList(int limit, String after);
 
+        Observable<List<RedditChildren>> getTopEntriesList(int limit, String after);
     }
 }
